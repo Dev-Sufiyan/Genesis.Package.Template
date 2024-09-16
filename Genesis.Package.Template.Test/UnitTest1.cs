1a342562cf1d613ac8f22d56448bdb7485c2abea
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-09-16 22:08:38 +0530

    Change in Test Project - Chnage to Xunit

--- Genesis.Package.Template.Test/UnitTest1.cs
+++ Genesis.Package.Template.Test/UnitTest1.cs
@@ -1,12 +1,11 @@
 namespace Genesis.Package.Template.Test
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public void Test1()
         {
-            Assert.AreEqual(1, 1);
+            Assert.Equal(1, 1);
         }
     }
 }
